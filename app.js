2389134860e54d2f39061316befc42c3442005e9
june850713@gmail.com <june850713@gmail.com>
2022-05-30 15:12:13 +0800

    modify index page

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('url', req.body.url)
   res.render('index')
 })
 
