5a1c1feb5ad87a973c724fc2226ac740056948f9
june850713@gmail.com <june850713@gmail.com>
2022-05-31 12:18:09 +0800

    modify mongoose Schema

--- models/shortURL.js
+++ models/shortURL.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const shortURLSchema = new Schema ({
   origin_URL: {
-    type: URL,
+    type: String,
     required: true
   },
   short_URL: {
-    type: URL,
+    type: String,
     required: true
   }
 })
