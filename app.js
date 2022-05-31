3633c19ef9fb9909af5058a49c12cb1421da2c4a
june850713@gmail.com <june850713@gmail.com>
2022-05-31 12:27:38 +0800

    add create function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const generateshortURL = require('./generateshortURL')
+const ShortURL = require('./models/shortURL')
 
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -18,8 +19,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
+  const originURL = req.body.url
   const shortURL = generateshortURL
-  res.render('shorturl', { shortURL })
+  ShortURL.create({ origin_URL: originURL, short_URL: shortURL})
+    .then(res.render('shorturl', { shortURL }))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
